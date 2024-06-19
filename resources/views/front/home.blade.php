d8efbc00c65d228bb347fc5640f195bd05e6ba49
ATTOLOU <regisattolou21@outlook.fr>
2024-06-19 21:56:20 +0200

    page home mise en place

--- resources/views/front/home.blade.php
+++ resources/views/front/home.blade.php
@@ -1,3 +1,10 @@
-<div>
-    Bienvenue sur la page d'accueil
-</div>
+@extends('front.layouts.app')
+
+@section('content')
+    @include('front.layouts.header')
+    @include('front.categories')
+    @include('front.layouts.slider')
+    @include('front.layouts.banner')
+    @include('front.product')
+    @include('front.layouts.footer')
+@endsection
--- resources/views/front/layouts/footer.blade.php
+++ resources/views/front/layouts/footer.blade.php
@@ -115,23 +115,23 @@
          <div class="p-t-40">
              <div class="flex-c-m flex-w p-b-18">
                  <a href="#" class="m-all-1">
-                     <img src="images/icons/icon-pay-01.png" alt="ICON-PAY">
+                     <img src="{{ asset('front_assets/images/icons/icon-pay-01.png') }}" alt="ICON-PAY">
                  </a>
 
                  <a href="#" class="m-all-1">
-                     <img src="images/icons/icon-pay-02.png" alt="ICON-PAY">
+                     <img src="{{ asset('front_assets/images/icons/icon-pay-02.png') }}" alt="ICON-PAY">
                  </a>
 
                  <a href="#" class="m-all-1">
-                     <img src="images/icons/icon-pay-03.png" alt="ICON-PAY">
+                     <img src="{{ asset('front_assets/images/icons/icon-pay-03.png') }}" alt="ICON-PAY">
                  </a>
 
                  <a href="#" class="m-all-1">
-                     <img src="images/icons/icon-pay-04.png" alt="ICON-PAY">
+                     <img src="{{ asset('front_assets/images/icons/icon-pay-04.png') }}" alt="ICON-PAY">
                  </a>
 
                  <a href="#" class="m-all-1">
-                     <img src="images/icons/icon-pay-05.png" alt="ICON-PAY">
+                     <img src="{{ asset('front_assets/images/icons/icon-pay-05.png') }}" alt="ICON-PAY">
                  </a>
              </div>
 
@@ -149,3 +149,10 @@
          </div>
      </div>
  </footer>
+
+ <!-- Back to top -->
+ <div class="btn-back-to-top" id="myBtn">
+     <span class="symbol-btn-back-to-top">
+         <i class="zmdi zmdi-chevron-up"></i>
+     </span>
+ </div>
